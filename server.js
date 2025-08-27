9480c7eb704da72de532a340dd97d19308e9603f
akashsharmax <sharmakash568@gmail.com>
2025-08-27 18:15:44 +0530

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
   // ...
