5f71e0bae573ef26c3cd880cb582e938de32bad0
akashsharmax <sharmakash568@gmail.com>
2025-08-28 12:54:20 +0530

    updated

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+require('dotenv').config();
 const app = require('./src/app');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateResponse = require('./src/service/ai.service');
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
 
-io.on("connection", (socket) => {
-  // ...
+const io = new Server(httpServer, {
+ 
 });
 
 
-httpserver.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}); 
+io.on("connection", (socket) => {
+  console.log(" User connected:", socket.id);
+
+  socket.on("ai-message", async (data) => {
+    console.log("Received AI message:", data.prompt);
+    const response = await generateResponse(data.prompt);
+    console.log("AI Response:", response);
+    socket.emit("ai-message-response", { response })
+  });
+
+  socket.on("disconnect", () => {
+    console.log(" User disconnected:", socket.id);
+  })
+});
+
+
+httpServer.listen(3000, () => {
+  console.log(' Server is running on port 3000');
+});
---
+++ src/service/ai.service.js
@@ -0,0 +1,23 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function generateResponse(prompt) {
+  try {
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+   
+    const result = await model.generateContent(prompt);
+
+   
+    return result.response.text();
+  } catch (err) {
+    console.error(" Error in generateResponse:", err);
+    return "Something went wrong while generating response.";
+  }
+}
+console.log("🔑 API Key Loaded:", process.env.GEMINI_API_KEY);
+
+module.exports = generateResponse;
